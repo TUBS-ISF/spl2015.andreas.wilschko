2ea242ef093dae34c8e1c943f4946ce67e62c717
Andreas <florin1andy@yahoo.de>
2015-05-12 18:56:23 +0200

    first implementations

--- Andreas-Wilschko-Task2/src/ServiceConfigurationBuilder.java
+++ Andreas-Wilschko-Task2/src/ServiceConfigurationBuilder.java
@@ -1,4 +1,32 @@
+import net.sf.json.JSONObject;
+
+
 
 public class ServiceConfigurationBuilder {
 	
+	private enum configOptions{
+		BuildOption,Create_Account,unsaved,unsecure,two_Persons
+	}
+
+	static JSONObject configurationJSON = new JSONObject();
+	public static void main(String[] args){
+		if(args.length==1){
+			configurationJSON.put("BuildOption",args[0]);
+		}
+	}
+	
+	private void configServerAndClient(){
+		
+	}
+	
+	private void configClient(){
+		
+	}
+	
+	private createOutputFile(){
+		
+	}
+	
+	
 }
+
