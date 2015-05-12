03026fab5c51e8b074bb3bd6b1130b554806156a
Andreas <florin1andy@yahoo.de>
2015-05-12 20:04:02 +0200

    config builder für features erstellt

--- Andreas-Wilschko-Task2/src/ServiceConfigurationBuilder.java
+++ Andreas-Wilschko-Task2/src/ServiceConfigurationBuilder.java
@@ -1,32 +1,52 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 import net.sf.json.JSONObject;
 
-
-
 public class ServiceConfigurationBuilder {
-	
-	private enum configOptions{
-		BuildOption,Create_Account,unsaved,unsecure,two_Persons
+
+	private static String[] argsCopy;
+
+	private enum configOptions {
+		Create_Account, unsaved, unsecure, two_Persons
 	}
 
 	static JSONObject configurationJSON = new JSONObject();
-	public static void main(String[] args){
-		if(args.length==1){
-			configurationJSON.put("BuildOption",args[0]);
+
+	public static void main(String[] args) {
+		argsCopy = args;
+		if (args.length == 0) {
+			System.out.println("You didn't add any arguments. Retry!");
+			System.exit(0);
 		}
 	}
-	
-	private void configServerAndClient(){
-		
-	}
-	
-	private void configClient(){
-		
-	}
-	
-	private createOutputFile(){
-		
-	}
-	
-	
-}
 
+	/**
+	 * this method builds the file that contains the Informations about active
+	 * features
+	 */
+	private void createConfig() {
+		for(int i=0;i<argsCopy.length;i++){
+				configurationJSON.put(argsCopy[i],"true");
+		}
+		createOutputFile(configurationJSON.toString());
+	}
+
+	private boolean createOutputFile(String configOutput) {
+		File configOutputFile = new File ("config.json");
+		boolean successfullyWritten=false;
+		try {
+			FileWriter fileWriter = new FileWriter(configOutputFile,false);
+			fileWriter.append(configOutput);
+			fileWriter.flush();
+			successfullyWritten=true;
+			fileWriter.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return successfullyWritten;
+	}
+
+}
